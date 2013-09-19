83daa6edd2bacc457b82d20d88005e348b0ae7df
alexkowa <alex@kowarik.net>
2013-09-19 13:31:29 +0200

    new cran checks for dependencies and imports

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,5 @@
 vmGUIenvir <- new.env()
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("VIM is ready to use. \n Since version 4.0.0 the GUI is in its own package VIMGUI.\n
+Please use the package to use the new (and old) GUI.\n") 
+}
