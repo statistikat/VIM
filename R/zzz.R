a0d0874db81f7b5a436175c65d5f2ed2948d73ba
alexkowa <alex@kowarik.net>
2013-12-09 07:27:11 +0100

    link to https://github.com/alexkowa/VIM/issues in startup message

--- R/zzz.R
+++ R/zzz.R
@@ -21,7 +21,8 @@
 vmGUIenvir <- new.env()
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("VIM is ready to use. \n Since version 4.0.0 the GUI is in its own package VIMGUI.\n
-          Please use the package to use the new (and old) GUI.\n") 
+          Please use the package to use the new (and old) GUI.\n")
+  packageStartupMessage("Suggestions and bug-reports can be submitted at: https://github.com/alexkowa/VIM/issues")
 }
 
 ## utility functions for GUI environment
