fad10ac49bb709d86e1cacf1f5154c60ad059f94
alexkowa <alex@kowarik.net>
2014-09-24 13:18:15 +0200

    working data.table version

--- R/hotdeck.R
+++ R/hotdeck.R
@@ -25,10 +25,10 @@
 #' @param ord_var variables for sorting the data set before imputation
 #' @param domain_var variables for building domains and impute within these
 #' domains
-#' @param makeNA vector of values, that should be converted to NA
-#' @param NAcond a condition for imputing a NA
+#' @param makeNA list of length equal to the number of variables, with values, that should be converted to NA for each variable
+#' @param NAcond list of length equal to the number of variables, with a condition for imputing a NA
 #' @param impNA TRUE/FALSE whether NA should be imputed
-#' @param donorcond condition for the donors e.g. ">5"
+#' @param list of length equal to the number of variables, with a donorcond condition for the donors e.g. ">5"
 #' @param imp_var TRUE/FALSE if a TRUE/FALSE variables for each imputed
 #' variable should be created show the imputation status
 #' @param imp_suffix suffix for the TRUE/FALSE variables showing the imputation
@@ -107,7 +107,7 @@ hotdeck_work <- function(data, variable=NULL, ord_var=NULL,domain_var=NULL,
   
   if(!is.null(makeNA)){
     if(length(makeNA)!=nvar)
-      stop("The vector 'variable' must have the same length as the 'makeNA' list")
+      stop("The list 'variable' must have the same length as the 'makeNA' list")
     else{
       for(i in 1:nvar){
         data[data[,variable[i]]%in%makeNA[[i]],variable[i]] <- NA 
@@ -215,85 +215,132 @@ hotdeck_work <- function(data, variable=NULL, ord_var=NULL,domain_var=NULL,
   datax
 }
 
-###TEST
-#set.seed(132)
-#nRows <- 1e6
-#x<-data.frame(x=rnorm(nRows),y=rnorm(nRows),z=sample(LETTERS,nRows,rep=T),
-#    d1=sample(LETTERS[1:3],nRows,rep=T),d2=sample(LETTERS[1:2],nRows,rep=T),o1=rnorm(nRows),o2=rnorm(nRows),o3=rnorm(100))
-#origX <- x
-#x[sample(1:nRows,nRows/10),1] <- NA
-#x[sample(1:nRows,nRows/10),2] <- NA
-#x[sample(1:nRows,nRows/10),3] <- NA
-#x[sample(1:nRows,nRows/10),4] <- NA
-#
-#hd <- function(x , variable=NULL, ord_var=NULL,domain_var=NULL){
-#  classx <- class(x)
-#  VariableSorting <- colnames(x)
-#  x$OriginalSortingVariable <- 1:nrow(x)
-#  x <- data.table(x)
-#  if(is.null(variable)){
-#    variable  <- colnames(x)[apply(is.na(x),2,any)]
-#  }
-#  ## xx should be a data.table and ord_var the name of variables to sort
-#  imputeHD <- function(xx,ord_var=ord_var,varType=varType){
-#    setkeyv(xx,ord_var)
-#    xx$UniqueIdForImputation <- 1:nrow(xx)
-#    
-#    for(v in variable){
-#      setkeyv(xx,v)
-#      if(varType[v]%in%c("numeric","integer")){
-#        impPart <- xx[J(NA_real_),UniqueIdForImputation,nomatch=FALSE]$UniqueIdForImputation
-#      }else{
-#        impPart <- xx[J(NA_character_),UniqueIdForImputation,nomatch=FALSE]$UniqueIdForImputation
-#      }
-#      if(length(impPart)>0){
-#        #pool <- 1:nrow(xx)
-#        #pool <- pool[-impPart]
-#        #if(length(pool)<0){
-#        #  break
-#        #}
-#        impDon <- impPart-1
-#        impDon[impDon<1] <- impPart[impDon<1]+1
-#        setkey(xx,UniqueIdForImputation)
-#        Don <- data.frame(xx[impDon,v,with=FALSE])[,1]
-#        TFindex <- is.na(Don)
-#        TF <- any(TFindex)
-#        if(TF){
-#          add <- 2
-#          while(TF){
-#            impDon[TFindex] <- impPart[TFindex]-add
-#            impDon[TFindex][impDon[TFindex]<1] <- impPart[TFindex][impDon[TFindex]<1]+add
-#            impDon2 <- impDon[TFindex]
-#            Don[TFindex] <- data.frame(xx[impDon2,v,with=FALSE])[,1]
-#            TFindex[TFindex] <- is.na(Don[TFindex])
-#            TF <- any(TFindex)
-#            if(add>50){
-#              TF <- FALSE
-#              Don[TFindex]<-1
-#            }
-#            add <- add +1
-#          }
-#        }
-#        #setkey(xx,UniqueIdForImputation)
-#        xx[impPart,v] <- Don
-#      }
-#    }
-#    xx[,UniqueIdForImputation:=NULL]
-#    setkey(xx,OriginalSortingVariable)
-#    xx
-#  }
-#  varType <- sapply(x,class)[variable]
-#  x <- x[,imputeHD(.SD,ord_var=ord_var,varType=varType), by = domain_var]
-#  setkey(x,OriginalSortingVariable)
-#  x[,OriginalSortingVariable:=NULL]
-#  if(all(classx!="data.table"))
-#    return(data.frame(x)[,VariableSorting])
-#  return(x[,VariableSorting,with=F])
-#}
+hotdeck_work2 <- function(x , variable=NULL, ord_var=NULL,domain_var=NULL,
+    makeNA=NULL,NAcond=NULL,impNA=TRUE,donorcond=NULL,
+    imp_var=TRUE,imp_suffix="imp"
+    ){
+  if(is.null(variable)){
+    variable <- colnames(x)
+    variable<-variable[!variable%in%c(ord_var,domain_var)]
+  }
+  if(!is.null(makeNA)){
+    if(!is.list(makeNA)||!length(makeNA)==length(variable))
+      stop("makeNA is not defined correctly")
+  }
+  classx <- class(x)
+  VariableSorting <- colnames(x)
+  x$OriginalSortingVariable <- 1:nrow(x)
+  x <- data.table(x)
+  if(is.null(variable)){
+    variable  <- colnames(x)[apply(is.na(x),2,any)]
+  }
+  if(!is.null(NAcond))
+    warning("NAcond is not implemented yet and will be ignored.")
+  ## xx should be a data.table and ord_var the name of variables to sort
+  imputeHD <- function(xx,variableX,ord_varX,varTypeX,imp_varX,imp_suffixX,
+      impNAX,makeNAX){
+    setkeyv(xx,ord_varX)
+    xx$UniqueIdForImputation <- 1:nrow(xx)
+    
+    for(v in variableX){
+      setkeyv(xx,v)
+      if(imp_varX){
+        xx[,impvar:=FALSE]
+      }
+      if(!impNAX){
+        if(is.null(makeNAX))
+          stop("If impNA=FALSE a list of values to be imputed must be provided.")
+        ## NAs should not be imputed
+        if(varTypeX[v]%in%c("numeric","integer")){
+          NAs <- xx[J(NA_real_),UniqueIdForImputation,nomatch=FALSE]$UniqueIdForImputation
+        }else{
+          NAs <- xx[J(NA_character_),UniqueIdForImputation,nomatch=FALSE]$UniqueIdForImputation
+        }
+        xxna <- xx[NAs]
+        xx <- xx[-NAs]
+      }
+      if(!is.null(makeNAX)){
+        eval(parse(text="xx[x>1]"))
+        setnames(xx,v,"weirdandlongname")
+        xx[weirdandlongname%in%makeNAX[[match(v,variableX)]],weirdandlongname:=NA]
+        setnames(xx,"weirdandlongname",v)
+      }
+      if(varTypeX[v]%in%c("numeric","integer")){
+        impPart <- xx[J(NA_real_),UniqueIdForImputation,nomatch=FALSE]$UniqueIdForImputation
+        if(imp_varX){
+          impvar <- list(TRUE)
+          names(impvar) <- paste()
+        }
+      }else{
+        impPart <- xx[J(NA_character_),UniqueIdForImputation,nomatch=FALSE]$UniqueIdForImputation
+      }
+      if(length(impPart)>0){
+        if(imp_varX){
+          impvarname <- paste(v,"_",imp_suffixX,sep="")
+          xx[impPart,impvar:=TRUE]
+          setnames(xx,"impvar",impvarname)
+        }
+        #pool <- 1:nrow(xx)
+        #pool <- pool[-impPart]
+        #if(length(pool)<0){
+        #  break
+        #}
+        impDon <- impPart-1
+        impDon[impDon<1] <- impPart[impDon<1]+1
+        setkey(xx,UniqueIdForImputation)
+        Don <- data.frame(xx[impDon,v,with=FALSE])[,1]
+        TFindex <- is.na(Don)
+        TF <- any(TFindex)
+        if(TF){
+          add <- 2
+          while(TF){
+            impDon[TFindex] <- impPart[TFindex]-add
+            impDon[TFindex][impDon[TFindex]<1] <- impPart[TFindex][impDon[TFindex]<1]+add
+            impDon2 <- impDon[TFindex]
+            Don[TFindex] <- data.frame(xx[impDon2,v,with=FALSE])[,1]
+            TFindex[TFindex] <- is.na(Don[TFindex])
+            TF <- any(TFindex)
+            if(add>50){
+              TF <- FALSE
+              Don[TFindex]<-1
+            }
+            add <- add +1
+          }
+        }
+        #setkey(xx,UniqueIdForImputation)
+        xx[impPart,v] <- Don
+      }
+      if(!impNAX)
+        xx <- rbind(xx,xxna)
+    }
+    xx[,UniqueIdForImputation:=NULL]
+    setkey(xx,OriginalSortingVariable)
+    xx
+  }
+  varType <- sapply(x,class)[variable]
+  x <- x[,imputeHD(.SD,variableX=variable,ord_varX=ord_var,varTypeX=varType,
+          imp_varX=imp_var,imp_suffixX=imp_suffix,impNAX=impNA,makeNAX=makeNA), by = domain_var]
+  setkey(x,OriginalSortingVariable)
+  x[,OriginalSortingVariable:=NULL]
+  if(all(classx!="data.table"))
+    return(data.frame(x)[,VariableSorting])
+  return(x[,VariableSorting,with=FALSE])
+}
 #require(data.table)
 #setwd("/Users/alex")
 #Rprof("profile1.out")
-#xImp <- hd(x,ord_var = c("o1","o2","o3"),domain_var="d2")
+###TEST
+#set.seed(132)
+nRows <- 1e6
+x<-data.frame(x=rnorm(nRows),y=rnorm(nRows),z=sample(LETTERS,nRows,rep=T),
+    d1=sample(LETTERS[1:3],nRows,rep=T),d2=sample(LETTERS[1:2],nRows,rep=T),o1=rnorm(nRows),o2=rnorm(nRows),o3=rnorm(100))
+origX <- x
+x[sample(1:nRows,nRows/10),1] <- NA
+x[sample(1:nRows,nRows/10),2] <- NA
+x[sample(1:nRows,nRows/10),3] <- NA
+x[sample(1:nRows,nRows/10),4] <- NA
+#
+xImp <- hotdeck_work2(x,ord_var = c("o1","o2","o3"),domain_var="d2")
 #Rprof(NULL)
 #summaryRprof("profile1.out")
 #xImp1 <- hotdeck(x,ord_var = c("o1","o2","o3"),domain_var="d2")
