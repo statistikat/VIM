6c6d717b72089863a8eb3bdb33c61f603f368069
alexkowa <alex@kowarik.net>
2017-07-19 09:48:02 +0100

    improve knn Test

--- tests/testthat/test_kNN.R
+++ tests/testthat/test_kNN.R
@@ -5,6 +5,7 @@ setna <- function(d,i,col=2){
   d[i,col] <- NA
   d
 }
+wm <- function(x,weights)weighted.mean(x,weights)
 ## Tests for k==1
 test_that("kNN Tests for k==1",{
   d1 <- kNN(setna(d,7:12,1)[,1:2],k=1)
@@ -20,7 +21,25 @@ test_that("kNN Tests for k==1",{
 d <- rbind(d,d[1:6,])
 d[13:18,2] <- d[13:18,2]*2
 test_that("kNN Tests for k==2",{
-  d2 <- kNN(setna(d,7:12,2)[,1:2],k=2,numFun = mean)
+  d2 <- kNN(setna(d,7:12,2)[,1:2],k=2,numFun = mean, useImputedDist = FALSE)
   expect_false(!all(d2[7:12,2]==(d2[1:6,2]+d2[13:18,2])/2),info=
                  "kNN does not work as supposed.")
+})
+
+d$m <- rnorm(6)
+d$m[1:2] <- 0
+## Tests for k==5
+test_that("kNN Tests for k==5 with mixed variable",{
+  d <- setna(d,7:12,2)
+  d <- setna(d,1:2,1)
+  d <- setna(d,3:4,5)
+  d2 <- kNN(d,k=5,numFun = wm,weightDist = TRUE, catFun = sampleCat, mixed = "m",
+            addRandom = TRUE)
+  
+})
+
+test_that("kNN Tests donorcond",{
+  d <- setna(d,7:12,2)
+  d2 <- kNN(d,k=5,donorcond = list(">3"),variable = "y")
+  expect_true(all(d2[d2$y_imp,"y"]>3))
 })
