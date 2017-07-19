f1ad8b28d5e8d64c178303d7997b90add3c3a898
alexkowa <alex@kowarik.net>
2017-07-19 09:36:35 +0100

    test improve irmi

--- tests/testthat/test_IRMI_ordered.R
+++ tests/testthat/test_IRMI_ordered.R
@@ -2,7 +2,30 @@
 # 
 # Author: Alexander Kowarik (Issue 11 Deleetdk)
 ###############################################################################
-test_df = data.frame(ord = ordered(sample(c(letters[1:2], NA), size = 1000, replace = T)), v1 = rnorm(1000), v2 = rnorm(1000))
-test_that("irmi should work on orderd factors",{
-  expect_is(irmi(test_df),"data.frame")
+test_df = data.frame(ord = ordered(sample(c(letters[1:2], NA), size = 1000, replace = T)), v1 = rnorm(1000), v2 = rnorm(1000), m=rnorm(1000),
+                     b=sample(LETTERS[1:2],1000,replace=TRUE),
+                     c=sample(LETTERS[1:5],1000,replace=TRUE),co=rpois(1000,10))
+test_df[sample(1000,100),"m"] <- 0
+test_that("irmi base",{
+  expect_is(irmi(test_df, mixed = "m", count="co"),"data.frame")
+})
+
+test_that("irmi step",{
+  expect_is(irmi(test_df, mixed = "m", count="co",step=TRUE),"data.frame")
+})
+
+test_that("irmi step noise",{
+  expect_is(irmi(test_df, mixed = "m", count="co",step=TRUE, noise=TRUE),"data.frame")
+})
+
+test_that("irmi robust",{
+  expect_is(irmi(test_df,robust=TRUE,mixed = "m", count="co"),"data.frame")
+})
+
+test_that("irmi robust step",{
+  expect_error(irmi(test_df,robust=TRUE,mixed = "m", count="co", step=TRUE))
+})
+
+test_that("irmi robust noise",{
+  expect_is(irmi(test_df,robust=TRUE,mixed = "m", count="co", noise=TRUE),"data.frame")
 })
