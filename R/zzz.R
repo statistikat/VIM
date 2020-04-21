d3414a3b37846cd31bb5dfcb4278806c852dce6e
Gregor de Cillia <de.cillia.gregor@gmail.com>
2020-04-21 09:29:45 +0200

    data.table: depends to imports

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
+#' @import data.table
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("VIM is ready to use.\n")
   packageStartupMessage("Suggestions and bug-reports can be submitted at: https://github.com/statistikat/VIM/issues")
