5c2d66c950f1b1138697d9cb7f134405a975aa56
Alexander Kowarik <alex@kowarik.net>
2026-03-08 20:36:03 +0100

    openmp version of kNN

--- src/gowerD.cpp
+++ src/gowerD.cpp
@@ -1,6 +1,12 @@
 #include <Rcpp.h>
 #include <string>
 #include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 
 void R_init_VIM(DllInfo* info) {
 	R_registerRoutines(info, NULL, NULL, NULL, NULL);
@@ -33,26 +39,41 @@ double distW(double x,double y, int type, double weight=1, double weightsum=1,do
 //  Rprintf("xval %f, yval %f, dist %f \n",x,y,out);
   return out;
 }
-double distW1(NumericVector xV,NumericVector yV, NumericVector weight,
-              NumericVector levOrder,
-              double ncolMAX,NumericVector ncolVAR,NumericVector mixedConstant){
+double distW1(const NumericMatrix& xMat, const NumericMatrix& yMat, int i, int j,
+              const NumericVector& weight, const NumericVector& levOrder,
+              double ncolMAX, const NumericVector& ncolVAR, const NumericVector& mixedConstant){
   double out=0;
   double weightsum = accumulate( weight.begin(), weight.end(), 0.0 );
   // compute the distance contribution of each variable
   for (int k=0; k<ncolMAX; k++) {
-    
     if(k<ncolVAR(0)){ //NUMERIC
-      out+=distW(xV(k),yV(k), 0, weight(k), weightsum);
+      out+=distW(xMat(i, k), yMat(j, k), 0, weight(k), weightsum);
     }else if(k<(ncolVAR(0)+ncolVAR(1))){ //Categorical
-      out+=distW(xV(k),yV(k), 1, weight(k), weightsum);
+      out+=distW(xMat(i, k), yMat(j, k), 1, weight(k), weightsum);
     }else if(k<(ncolVAR(0)+ncolVAR(1)+ncolVAR(2))){  //Ordered
-      out+=distW(xV(k),yV(k), 2, weight(k), weightsum,levOrder(k-(ncolVAR(0)+ncolVAR(1))));
+      out+=distW(xMat(i, k), yMat(j, k), 2, weight(k), weightsum,levOrder(k-(ncolVAR(0)+ncolVAR(1))));
     }else if(k<(ncolVAR(0)+ncolVAR(1)+ncolVAR(2)+ncolVAR(3))){  //Semi-Continous
-      out+=distW(xV(k),yV(k), 3, weight(k), weightsum,1,mixedConstant(k-(ncolVAR(0)+ncolVAR(1)+ncolVAR(2))));
+      out+=distW(xMat(i, k), yMat(j, k), 3, weight(k), weightsum,1,mixedConstant(k-(ncolVAR(0)+ncolVAR(1)+ncolVAR(2))));
     }
-    
   }
   return out;
+}
+
+void whichminN_impl(std::vector<double>& x, int n, double* out, double* outMin=nullptr) {
+  std::vector<double>::iterator it = min_element(x.begin(), x.end());
+  out[0] = static_cast<double>(it - x.begin() + 1);
+  if (outMin != nullptr) {
+    outMin[0] = x[it - x.begin()];
+  }
+  x[it - x.begin()] = R_PosInf;
+  for (int i=2; i<=n; i++) {
+    it = min_element(x.begin(), x.end());
+    out[i-1] = static_cast<double>(it - x.begin() + 1);
+    if (outMin != nullptr) {
+      outMin[i-1] = x[it - x.begin()];
+    }
+    x[it - x.begin()] = R_PosInf;
+  }
 }
 // [[Rcpp::export]]
 RcppExport SEXP gowerd(SEXP dataX, SEXP dataY,SEXP weights,SEXP ncolNUMFAC,
@@ -68,10 +89,11 @@ RcppExport SEXP gowerd(SEXP dataX, SEXP dataY,SEXP weights,SEXP ncolNUMFAC,
   int ny = yMat.nrow();
   NumericMatrix delta(nx,ny);
   double ncolMAX = ncolVAR(0)+ncolVAR(1)+ncolVAR(2)+ncolVAR(3);
+#pragma omp parallel for collapse(2) if (nx * ny > 1)
   for (int i=0; i<nx; i++) {
     for (int j=0; j<ny; j++) {
-        delta(i,j)=distW1(xMat(i,_),yMat(j,_),weight,levOrder,
-              ncolMAX,ncolVAR,mixedConstant);
+        delta(i,j)=distW1(xMat, yMat, i, j, weight, levOrder,
+              ncolMAX, ncolVAR, mixedConstant);
     }
   }
 
@@ -89,29 +111,16 @@ RcppExport SEXP whichminN(SEXP xR, SEXP nR, int returnValue) {
   NumericVector x(xR);  // creates Rcpp matrix from SEXP
   int n = as<int>(nR);
   NumericVector out(n);
-  NumericVector::iterator it = min_element(x.begin(), x.end());  // STL algo // iterator type
-  out[0]=it - x.begin()+1;
+  std::vector<double> xCopy(x.begin(), x.end());
   if(returnValue==1){
     NumericVector outMin(n);
-    outMin[0]=x[it - x.begin()];
-    x[it - x.begin()]=R_PosInf;
-    for (int i=2; i<=n; i++) {
-      it = min_element(x.begin(), x.end());
-      out[i-1]=it - x.begin()+1;
-      outMin[i-1]=x[it - x.begin()];
-      x[it - x.begin()]=R_PosInf;
-    }
+    whichminN_impl(xCopy, n, out.begin(), outMin.begin());
     return List::create(
         Named( "which" ) = out,
         Named( "mins" ) = outMin
     ) ;
   }else{
-    x[it - x.begin()]=R_PosInf;
-    for (int i=2; i<=n; i++) {
-      it = min_element(x.begin(), x.end());
-      out[i-1]=it - x.begin()+1;
-      x[it - x.begin()]=R_PosInf;
-    }
+    whichminN_impl(xCopy, n, out.begin());
     return List::create(
         Named( "which" ) = out
     ) ;
@@ -130,21 +139,28 @@ RcppExport SEXP gowerDind(SEXP dataX, SEXP dataY,SEXP weights,SEXP ncolNUMFAC,SE
   int returnMin = as<int>(returnMinR);
   NumericMatrix inds(n,nc);
   if(returnMin==0){
+#pragma omp parallel for if (nc > 1)
     for (int i=0; i<nc; i++) {
-      NumericVector zz1 = delta( Rcpp::_, i);
-      List resultList(whichminN(zz1,nR,returnMin));
-      inds(_,i)=as<NumericVector>(resultList["which"]);
+      std::vector<double> column(delta.rows());
+      std::copy(delta.begin() + static_cast<R_xlen_t>(i) * delta.rows(),
+                delta.begin() + static_cast<R_xlen_t>(i + 1) * delta.rows(),
+                column.begin());
+      whichminN_impl(column, n, inds.begin() + static_cast<R_xlen_t>(i) * n);
     }
     return List::create(
         Named( "ind" ) = inds
     );
   }else{
     NumericMatrix mins(n,nc);
+#pragma omp parallel for if (nc > 1)
     for (int i=0; i<nc; i++) {
-      NumericVector zz1 = delta(_, i);
-      List resultList(whichminN(zz1,nR,returnMin));
-      inds(_,i)=as<NumericVector>(resultList["which"]);
-      mins(_,i)=as<NumericVector>(resultList["mins"]);
+      std::vector<double> column(delta.rows());
+      std::copy(delta.begin() + static_cast<R_xlen_t>(i) * delta.rows(),
+                delta.begin() + static_cast<R_xlen_t>(i + 1) * delta.rows(),
+                column.begin());
+      whichminN_impl(column, n,
+                     inds.begin() + static_cast<R_xlen_t>(i) * n,
+                     mins.begin() + static_cast<R_xlen_t>(i) * n);
     }
     return List::create(
         Named( "ind" ) = inds,
